fd757dd68a1d8cae89b05dcb3d7b2081026af319
sheiza-jagemi <sheiza.jagemi@student.moringaschool.com>
2025-08-09 17:27:19 +0300

    first commit

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,40 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    approved_breeds = ["Mastiff", "Chihuahua", "Corgi", "Shar Pei", "Beagle", "French Bulldog", "Pug", "Pointer"]
+
+    def __init__(self, name="Fido", breed="Mastiff"):
+        self._name = None
+        self._breed = None
+        # Only validate name if it's not the default
+        if name != "Fido":
+            self.name = name
+        else:
+            self._name = name
+        # Only validate breed if it's not the default
+        if breed != "Mastiff":
+            self.breed = breed
+        else:
+            self._breed = breed
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        if isinstance(name, str) and 1 <= len(name) <= 25:
+            self._name = name
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+    @property
+    def breed(self):
+        return self._breed
+
+    @breed.setter
+    def breed(self, breed):
+        if breed in self.approved_breeds:
+            self._breed = breed
+        else:
+            print("Breed must be in list of approved breeds.")
--- lib/person.py
+++ lib/person.py
@@ -1,19 +1,42 @@
 #!/usr/bin/env python3
 
-APPROVED_JOBS = [
-    "Admin",
-    "Customer Service",
-    "Human Resources",
-    "ITC",
-    "Production",
-    "Legal",
-    "Finance",
-    "Sales",
-    "General Management",
-    "Research & Development",
-    "Marketing",
-    "Purchasing"
-]
-
 class Person:
-    pass
+    approved_jobs = ["Admin", "Customer Service", "Human Resources", "ITC", "Production", 
+                    "Legal", "Finance", "Sales", "General Management", 
+                    "Research & Development", "Marketing", "Purchasing"]
+
+    def __init__(self, name="John Doe", job="Admin"):
+        self._name = None
+        self._job = None
+        # Only validate name if it's not the default
+        if name != "John Doe":
+            self.name = name
+        else:
+            self._name = name.title()
+        # Only validate job if it's not the default
+        if job != "Admin":
+            self.job = job
+        else:
+            self._job = job
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        if isinstance(name, str) and 1 <= len(name) <= 25:
+            self._name = name.title()
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+    @property
+    def job(self):
+        return self._job
+
+    @job.setter
+    def job(self, job):
+        if job in self.approved_jobs:
+            self._job = job
+        else:
+            print("Job must be in list of approved jobs.")
